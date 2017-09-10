8b43e9d741aec3ffb4831dd2e34e3eb6fbfb95e2
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-10 20:56:08 +0530

    Use TileState class. Implement memory initialization

--- ivc.py
+++ ivc.py
@@ -1,9 +1,13 @@
 #! /usr/bin/python2
 
-from env import Env
+from env import Env, TileState
 
 class IVC(object):
 
 	def __init__(self, env, pos):
 		self.env = env
 		self.pos = [i for i in pos]
+
+
+	def memInit(self, env):
+		self.mem = [ [TileState.UNKNOWN]*env.dim[1] for i in xrange(env.dim[0]) ]
