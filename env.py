3385e9d63f5b07948c7eb166105235cd00ca11aa
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-10 09:34:49 +0530

    Create env.py, gui.py

---
+++ env.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python2
+
+class Env(object):
+	"""docstring for Env"""
+	def __init__(self, arg, dim, dirtRatio):
+		"""
+
+		"""
+		super(Env, self).__init__()
+		self.dim = dim
+		self.dirtRatio = dirtRatio
---
+++ gui.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python2
+
+import turtle
+
+class GUI(object):
+	"""This class implements the gui in turtle"""
+	def __init__(self, arg):
+		self.arg = arg
+	
+	
