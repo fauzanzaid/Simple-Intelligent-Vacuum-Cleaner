48c9cf46c2f9e4e314679c2b13dd8bdb5380f9d8
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-12 09:39:10 +0530

    Implement homeDistInit() which will find the minimum cost from current position to move to one of the homes

--- h1ctrlr.py
+++ h1ctrlr.py
@@ -5,7 +5,7 @@ from env import TileState
 from ivc import IVCAction
 
 class H1Controller(Controller):
-	
+
 	def __init__(self, dim, homes):
 		super(H1Controller, self).__init__(dim, homes)
 		self.homeDistInit(self.dim)
@@ -21,3 +21,18 @@ class H1Controller(Controller):
 
 	def homeDistInit(self, grid):
 		self.homeDist = [ [float("inf")]*self.dim[1] for i in xrange(self.dim[0]) ]
+
+		for i,row in enumerate(self.homeDist):
+			for j, v in enumerate(row):
+
+				minCost = float("inf")
+				for home in self.homes:
+					cost = self.costMoveCalc(home, [i,j])
+					if cost<minCost:
+						minCost = cost
+
+				self.homeDist[i][j] = minCost
+
+
+	def costMoveCalc(self, pos1, pos2):
+		return abs(pos1[0]-pos2[0]) + abs(pos1[1]-pos2[1])
