c84957d46fa894141cb5df40e089d782ec03a5b0
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-12 18:22:12 +0530

    Move home dist calculation to base class

--- ctrlr.py
+++ ctrlr.py
@@ -5,6 +5,23 @@ class Controller(object):
 	def __init__(self, dim, homes):
 		self.dim = dim[:]
 		self.homes = [home[:] for home in  homes]
+		self.homeDist = None
+		self.homeDistInit(self.dim)
+
+
+	def homeDistInit(self, grid):
+		self.homeDist = [ [float("inf")]*self.dim[1] for i in xrange(self.dim[0]) ]
+
+		for i,row in enumerate(self.homeDist):
+			for j, v in enumerate(row):
+
+				minCost = float("inf")
+				for home in self.homes:
+					cost = self.costMoveCalc(home, [i,j])
+					if cost<minCost:
+						minCost = cost
+
+				self.homeDist[i][j] = minCost
 
 	
 	def output(self, grid, pos):
--- h1ctrlr.py
+++ h1ctrlr.py
@@ -8,7 +8,6 @@ class H1Controller(Controller):
 
 	def __init__(self, dim, homes):
 		super(H1Controller, self).__init__(dim, homes)
-		self.homeDistInit(self.dim)
 
 
 	def output(self, grid, pos):
@@ -17,21 +16,6 @@ class H1Controller(Controller):
 
 	def heuristic(self, grid, pos):
 		pass
-
-
-	def homeDistInit(self, grid):
-		self.homeDist = [ [float("inf")]*self.dim[1] for i in xrange(self.dim[0]) ]
-
-		for i,row in enumerate(self.homeDist):
-			for j, v in enumerate(row):
-
-				minCost = float("inf")
-				for home in self.homes:
-					cost = self.costMoveCalc(home, [i,j])
-					if cost<minCost:
-						minCost = cost
-
-				self.homeDist[i][j] = minCost
 
 
 	def costMoveCalc(self, pos1, pos2):
