4365e7eedf40484380813a5f57253865c7651bd2
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-12 09:31:23 +0530

    Add homeDistInit()

--- h1ctrlr.py
+++ h1ctrlr.py
@@ -7,10 +7,17 @@ from ivc import IVCAction
 class H1Controller(Controller):
 	
 	def __init__(self, dim, homes):
-		super(DFSController, self).__init__(dim, homes)
+		super(H1Controller, self).__init__(dim, homes)
+		self.homeDistInit(self.dim)
+
 
 	def output(self, grid, pos):
 		pass
 
+
 	def heuristic(self, grid, pos):
 		pass
+
+
+	def homeDistInit(self, grid):
+		self.homeDist = [ [float("inf")]*self.dim[1] for i in xrange(self.dim[0]) ]
